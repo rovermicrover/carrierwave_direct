dfddf37c55b82dbadd3fc20ac56934a5fc85ee92
Andrew Rove <rovermicrover@mac.com>
2015-03-18 23:04:28 -0500

    Try to fix issue with direct_fog_url(:with_path => true) returning url for old upload.

--- lib/carrierwave_direct/uploader/direct_url.rb
+++ lib/carrierwave_direct/uploader/direct_url.rb
@@ -4,7 +4,7 @@ module CarrierWaveDirect
 
       def direct_fog_url(options = {})
         if options[:with_path]
-          url
+          CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), self.key).url
         else
           CarrierWave::Storage::Fog::File.new(self, CarrierWave::Storage::Fog.new(self), nil).public_url
         end
